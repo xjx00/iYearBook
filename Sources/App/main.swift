2976863da470f462e6d4679284aee14a53593c8b
xjx00 <1504635269@qq.com>
2017-05-01 00:52:17 +0800

    Update main.swift

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,16 +1,17 @@
 import Vapor
 
-//let drop = Droplet()
-//drop.get { req in
- //   return try drop.view.make("welcome", [
- //   	"message": drop.localization[req.lang, "welcome", "title"]
- //   ])
-//}
-//drop.resource("posts", PostController())
 let drop = Droplet()
 
 drop.get("/") { req in
     return "Hello, world."
 }
 
+drop.get("/set") { request in
+    let name = request.data["name"]?.string
+    let age = request.data["age"]?.string
+    let phone = request.data["phone"]?.string
+    let qq = request.data["qq"]?.string
+    let address = request.data["address"]?.string
+    return "\(name)\(age)\(phone)\(qq)\(address)"
+                 }
 drop.run()
