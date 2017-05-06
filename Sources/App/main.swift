25f5b8db81fb8993888729d55e6f14b1abbd3b5d
xjx00 <1504635269@qq.com>
2017-05-06 22:54:05 +0800

    Update main.swift
    add debug

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,6 +2,15 @@ import Vapor
 //import HTTP
 let drop = Droplet()
 
+//start for debug
+let debug = true
+let name = "test"
+let age = 17
+let phone = "18612345678"
+let qq = "1504635269"
+let address = "universe"
+//end for debug
+
 drop.get("/") { req in
     return "Hello, yearbook. ver 2.1"
 }
