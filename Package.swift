165fbd9159ae6ab409103c8840e6d407637bce31
xjx00 <1504635269@qq.com>
2017-05-13 23:51:00 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/sfaxon/vapor-memory-provider.git", majorVersion: 1)
     ],
     exclude: [
