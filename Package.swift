aebca95f7c45c0400ec1d0ec7d85ede4e2eaec57
xjx00 <1504635269@qq.com>
2017-07-12 20:56:49 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/BrettRToomey/brett-xml.git", majorVersion: 0, minor: 2),
+        //.Package(url: "https://github.com/BrettRToomey/brett-xml.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/chenyunguiMilook/SwiftyXML.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/sfaxon/vapor-memory-provider.git", majorVersion: 1)
     
     ],
